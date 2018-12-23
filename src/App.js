2c4bd8f3d77273e9e8d2b6040ea9372ebaaba3bb
Dominic Shillingford <dshillingford77@gmail.com>
2018-12-23 13:18:40 -0700

    Refactored the NavBar component to separate the NavBar code from the Router code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+// React basics
 import React, { Component } from 'react';
-import NavBar from './Components/Navigation/NavBar';
-import logo from './Images/cog.png';
+
+// React libraries and components 
+import { Route, Switch } from 'react-router';
+
+// Website components
+import NavBar from "./Components/Navigation/NavBar"
+import Footer from "./Components/Footer/Footer"
+import FizzExecs from "./Components/Admin/FizzExecs/FizzExecs"
+import FAQ from "./Components/Services/FAQ/FAQ"
+import About from "./Components/About/About"
+import ScrollToTop from './Components/Navigation/ScrollToTop'
 
 class App extends Component {
 
     render() {
         return (
-            <div className="app-container" style={ { height: '100%' }}>
-                <NavBar logo={logo} />
-            </div>
+            <ScrollToTop>
+                <Route component={NavBar}/>
+
+                <div style={ { paddingTop: "10vh" } }>
+                    <Switch>
+                        <Route path="/executives" component={FizzExecs}/>
+                        <Route path="/faq" component={FAQ}/>
+                        <Route component={About}/>
+                    </Switch>
+                </div>
+
+                <Route component={Footer}/>
+            </ScrollToTop>
         );
     }
 }
