94d2180edc50f1b57216cb34a495f02c18d8b965
Dominic Shillingford <dshillingford77@gmail.com>
2018-12-23 11:29:45 -0700

    Added Announcement component
    Added an Announcement component to the main page to separate this functionality from the page itself

---
+++ src/Components/About/Announcements.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import StackGrid, { transitions } from "react-stack-grid";
+
+const { scaleDown } = transitions;
+
+const announcements = require( './Announcements.json' );
+
+class Announcements extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            announcements: announcements
+        }
+    }
+
+    parseAnnouncement( data ) {
+        return (
+            <Paper className="announcement-card" key={data.title}>
+                <h3>{data.title}</h3>
+                <p className="announcement-description" dangerouslySetInnerHTML={{ __html: data.description }}></p>
+            </Paper>
+        );
+    }
+
+    render() {
+        return ( 
+            <StackGrid
+            appear={scaleDown.appear}
+            appeared={scaleDown.appeared}
+            enter={scaleDown.enter}
+            entered={scaleDown.entered}
+            leaved={scaleDown.leaved}
+            columnWidth={300}>
+
+                { this.state.announcements.map( this.parseAnnouncement ) }
+
+            </StackGrid>
+        );
+    }
+}
+
+export default Announcements;
