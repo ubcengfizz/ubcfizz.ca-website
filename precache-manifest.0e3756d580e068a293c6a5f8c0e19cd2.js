425a7d235b2f0e5c951b2cf3ac66e793f3c4beaa
Charles Lee <chengche674@gmail.com>
2021-02-17 21:58:08 -0800

    Updates

---
+++ precache-manifest.0e3756d580e068a293c6a5f8c0e19cd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a817bf7fab44a64a95d2f76d48e1db08",
+    "url": "/fizz/index.html"
+  },
+  {
+    "revision": "038cfbc5481118a6ae52",
+    "url": "/fizz/static/css/main.d29f6cd6.chunk.css"
+  },
+  {
+    "revision": "f6d2c24e697a03b42713",
+    "url": "/fizz/static/js/2.5f49d924.chunk.js"
+  },
+  {
+    "revision": "bebc31454123a1d4a1062b089f8ae461",
+    "url": "/fizz/static/js/2.5f49d924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038cfbc5481118a6ae52",
+    "url": "/fizz/static/js/main.4aaedb91.chunk.js"
+  },
+  {
+    "revision": "d0f92b4704a8820945ee",
+    "url": "/fizz/static/js/runtime-main.4983071a.js"
+  },
+  {
+    "revision": "4f57deea14fdc71d144a6af8bd3b51aa",
+    "url": "/fizz/static/media/background2.4f57deea.png"
+  },
+  {
+    "revision": "20e54dcdc3a865e05a8fd487a8677af0",
+    "url": "/fizz/static/media/fizz-logo.20e54dcd.svg"
+  }
+]);
