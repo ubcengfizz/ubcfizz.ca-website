143253a58c56eeb25f565308673870d7f3e18fe4
Hod Kimhi <hodkimhi@gmail.com>
2022-06-22 23:07:24 -0700

    Updated positions

--- src/Components/Admin/Executives/Executives.js
+++ src/Components/Admin/Executives/Executives.js
@@ -8,48 +8,46 @@ class Executives extends Component {
         return (
             <PageTemplate shortTitle="Executives" fullTitle="Fizz Executives">
                 <Person 
-                    title="President" 
-                    name="Richard Echegaray"
+                    title="Co-Presidents" 
+                    name="Andrew Chen, Charles Lee"
                     email="president"
-                    // img = "http://placekitten.com/200/300"
-                    description="The president represents the interests of engineering physics students to the Physics & Astronomy department, faculty of Applied Science, Engineering Undergraduate Society and alumni. The president chairs executive meetings, oversees activities of the association and heads any extra projects put forward by the council."
+                    description="The co-presidents represent the interests of engineering physics students to the Physics & Astronomy department, faculty of Applied Science, Engineering Undergraduate Society and alumni. The co-presidents chair executive meetings, oversee activities of the association and head any extra projects put forward by the council."
                 />
 
-                <Person 
+                {/* <Person 
                     title="VP Academic" 
                     name="Michelle Li and Olivia Kim"
                     email="vpacademic"
                     //img = "http://placekitten.com/200/300"
                     description="The VP Academic is responsible for organizing events relating to the academic and professional development of engineering physics students. These include the Technology & Science Seminar Series, the Graduate School Information Night and the Fizz Tutoring Program for 1st year STEM students."
-                />
+                />*/}
 
                 <Person 
                     title="VP Events" 
-                    name="Alexi Garbuz, Beth Hanna, Olivia Kim, and Renu Rajamagesh"
+                    name="Anqi Xu, Aileen Zhang, Renu Rajamagesh"
                     email="vpevents"
-                    //img = "http://placekitten.com/200/300"
                     description="The VP Events ensures that students maintain a healthy social life by organizing and advertising a number of events throughout the year. Yearly events include Fizz Talks, Fire and Ice and Alumni Night."
                 />
 
                 <Person 
                     title="Treasurer" 
-                    name="Lesley Hill"
+                    name="Lesley Hill, Josh Lim"
                     email="treasurer"
                     //img = "http://placekitten.com/200/300"
                     description="The Treasurer handles all of the financial activities and transactions. This includes creating and balancing the budget, giving reimbursements, liaising with the AMS Finance office and acting as one of the two booking representatives for the club."
                 />      
 
-                <Person 
+                {/* <Person 
                     title="Graduate Representative" 
                     name="Lewis Mason and James Wu"
                     email="gradrep"
                     //img = "http://placekitten.com/200/300"
                     description="The graduate representative acts as a liaison between the executive team and the graduating class. They are responsible for informing students about the current activities of the club and selling tickets for pertinent events. In addition, they coordinate graduate photographs and ensure that a composite is produced, gather ring sizes from the graduates and help organize the UBC APSC Graduation Ceremony."
-                />   
+                />*/}
 
                 <Person 
                     title="Year Representatives" 
-                    name="Vacant / Aileen Zhang & Sabrina Ashik / Bridget Meyboom & Sylvia Zhang"
+                    name="Vacant / Husan Aulakh & Kyle Mackenzie / Kenneth Xing"
                     email="secondrep / thirdrep / fourthrep"
                     //img = "http://placekitten.com/200/300"
                     description="The year representatives act as liaisons between the executive team and the students of their respective years. They are responsible for informing students about the current activities of the club, selling tickets for pertinent events and helping to organize and execute many of the academic and social events."
@@ -57,7 +55,7 @@ class Executives extends Component {
 
                 <Person 
                     title="IT Manager" 
-                    name="Charles Lee, Thomas Deckers, and Michelle Li"
+                    name="Jackson Fraser, Hod Kimhi"
                     email="itmanager"
                     //img = "http://placekitten.com/200/300"
                     description="The IT manager is responsible for maintaining the Penthouse computers, wireless access and printers. They also update and oversee the ubcfizz.com website and mailing lists."
@@ -65,23 +63,23 @@ class Executives extends Component {
 
                 <Person 
                     title="Board of Studies Representatives" 
-                    name="Julia Rosenrauch and Calista Abuan"
+                    name="Michelle Li, Sylvia Zhang"
                     email="bos"
                     //img = "http://placekitten.com/200/300"
                     description="The two Board of Studies representatives organize feedback from current students and advocates for changes to the Engineering Physics curriculum. Feedback is primarily solicited at twice-annual Beef and Pizza events, in addition to mid-term evaluations conducted during class. In the first half of the academic year, the Board of Studies representatives bring collected feedback to a Board of Studies meeting with the Engineering Physics Director and various Engineering department heads to discuss curriculum changes."
                 />  
 
-                <Person 
+                {/* <Person 
                     title="Sports Representative" 
                     name="Sophie Varabioff"
                     email="sports"
                     //img = "http://placekitten.com/200/300"
                     description="The sports representative is responsible for communicating UBC REC leagues and event registration dates through the club mailing list. They facilitate the organization of REC league teams from interested members and organize an outdoor excursion for club members, such as white-water rafting, paint balling, or ski trips."
-                />  
+                />*/}
 
                 <Person 
                     title="Penthouse Manager" 
-                    name="Kenneth Xing"
+                    name="Spencer Bradbury, Pedro Amui"
                     email="penthouse"
                     //img = "http://placekitten.com/200/300"
                     description="The penthouse manager oversees the maintenance and all upgrades of the Engineering Physics
@@ -90,7 +88,7 @@ class Executives extends Component {
 
                 <Person 
                     title="E-Week Representative" 
-                    name="Andrew Chen"
+                    name="Ethan Predinchuk"
                     email="eweek"
                     //img = "http://placekitten.com/200/300"
                     description="The E-Week representative is responsible for organizing all things pertaining to E-Week. This involves going to EUS meetings in preparation for the week as well as promoting the events and allocating resources to promote a victory for the department."
